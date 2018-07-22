b774bfde5330fee628abfcade0c1e3e05675e599
LJYLeo <wxliujiayu23@qq.com>
2018-07-22 20:54:09 +0800

    创建后台工程

---
+++ src/main/java/org/apache/edgent/edgentsamplesadmin/EdgentSamplesAdminApplication.java
@@ -0,0 +1,12 @@
+package org.apache.edgent.edgentsamplesadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgentSamplesAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgentSamplesAdminApplication.class, args);
+	}
+}
